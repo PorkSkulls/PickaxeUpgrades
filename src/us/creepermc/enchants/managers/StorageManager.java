6629bf53eeea6ccae23d90a92ffa3e9a614ed5d8
EmberCM <kooly213@hotmail.com>
2020-09-08 21:53:49 -0500

    actually save the stored energy

--- src/us/creepermc/enchants/managers/StorageManager.java
+++ src/us/creepermc/enchants/managers/StorageManager.java
@@ -2,9 +2,15 @@ package us.creepermc.enchants.managers;
 
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import us.creepermc.enchants.Core;
 import us.creepermc.enchants.templates.XManager;
+import us.creepermc.mines.utils.Files;
 
+import java.io.FileReader;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -15,6 +21,7 @@ public class StorageManager extends XManager {
 	final Map<UUID, Long> energy = new HashMap<>();
 	double expChance;
 	int expAmount;
+	int saveTask;
 	
 	public StorageManager(Core core) {
 		super(core);
@@ -22,16 +29,47 @@ public class StorageManager extends XManager {
 	
 	@Override
 	public void initialize() {
-		deinitialize();
+		if(saveTask != 0) getCore().getServer().getScheduler().cancelTask(saveTask);
+		if(!energy.isEmpty()) saveJSON("storage.json");
+		energy.clear();
 		
 		expChance = getCore().getConfig().getDouble("pickaxe-exp.chance") / 100;
 		expAmount = getCore().getConfig().getInt("pickaxe-exp.amount");
+		loadJSON("storage.json");
+		saveTask = new BukkitRunnable() {
+			@Override
+			public void run() {
+				saveJSON("storage.json");
+			}
+		}.runTaskTimerAsynchronously(getCore(), 36000, 36000 /* 30 minutes */).getTaskId();
 	}
 	
 	@Override
 	public void deinitialize() {
+		if(saveTask != 0) getCore().getServer().getScheduler().cancelTask(saveTask);
+		saveJSON("storage.json");
+		energy.clear();
 		expChance = 0;
 		expAmount = 0;
+	}
+	
+	private void loadJSON(String file) {
+		try {
+			JSONObject json = (JSONObject) new JSONParser().parse(new FileReader(Files.getFile(getCore(), file, true)));
+			((HashMap<String, Long>) json).forEach((k, v) -> energy.put(UUID.fromString(k), v));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void saveJSON(String file) {
+		JSONObject all = new JSONObject();
+		energy.forEach((uuid, amount) -> all.put(uuid.toString(), amount));
+		try {
+			java.nio.file.Files.write(Paths.get(getCore().getDataFolder().getPath() + "/" + file), all.toJSONString().getBytes());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void blockBreak(UUID uuid) {
